d17640daa885802a2d9bac2c4eaf5517db6f44ab
Rafi Ahmed <rafiprogrammer>
2025-03-01 03:09:40 +0600

    layout design

---
+++ script/function.js
@@ -0,0 +1,10 @@
+// document.getElementsById("click").addEventListener("click", function (event) {
+//   console.log("hello")
+//   });
+
+  // event.preventDefault()
+  //   const taskAssign = document.getElementById('task-assign').innerText
+  //   const task = parseInt(taskAssign)
+  //   sum = task - 1
+  //    document.getElementById('task-assign').innerText = sum
+
